e56a67770a23b35d87ee233bfb58322265d1f1fa
jdmax15 <joel.maxwell15@gmail.com>
2024-09-03 10:11:33 +0930

    added circleshape.py

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import pygame
 from constants import *
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -16,8 +19,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill((0,0,0), rect=None)
+        screen.fill((0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
